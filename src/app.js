df51c0c43c5a1cbd8c18c47323458510ce5ec1a7
YannikMG <yannik.giebenhain@web.de>
2022-01-06 02:48:43 +0100

    Added shutdown option for real now

--- src/app.js
+++ src/app.js
@@ -9,25 +9,42 @@ const port = process.env.PORT || 3000;
 const broadcast = process.env.BROADCAST || '10.10.0.255';
 const macaddress = process.env.MACADDRESS || '00:00:00:00:00:00';
 const redirectWebpage = process.env.REDIRECTWEBPAGE || 'https://www.google.com/';
-const timeToRedirect = process.env.TIMETOREDIRECT || 7;
+const timeToRedirect = process.env.TIMETOREDIRECT || 35;
 const host = process.env.HOST;
 const sshFile = '/ssh/identity';
 
 if (typeof host !== 'undefined' && host) {
-    app.get('/shutdown', (req, res) => {
-        exec(`ssh -oStrictHostKeyChecking=accept-new -i ${sshFile} root@${host} shutdown now`, (error, stdout, stderr) => {
+    app.get('/shutdown', async(req, res) => {
+        exec(`ssh -oBatchMode=yes -oStrictHostKeyChecking=accept-new -i ${sshFile} root@${host} shutdown now`, (error, stdout, stderr) => {
             if (error) {
                 console.log(`error: ${error.message}`);
-                res.send("There was an error while trying to shutdown the Server please contact the administrator!");
                 return;
             }
             if (stderr) {
                 console.log(`stderr: ${stderr}`);
-                res.sendFile('public/shutdown.html');
                 return;
             }
             console.log(`stdout: ${stdout}`);
-            res.sendFile('public/shutdown.html');
+        });
+        await sleep(5000);
+        res.redirect('/shutdown2');
+    });
+
+    app.get('/shutdown2', (req, res) => {
+        exec(`nc -vz ${host} 22`, (error, stdout, stderr) => {
+            if (error) {
+                // The PC is off
+                console.log(`error: ${error.message}`);
+                res.sendFile(path.join(__dirname, 'public/', 'shutdown.html'));
+                return;
+            }
+            if (stderr) {
+                // The PC is still on
+                console.log(`stderr: ${stderr}`);
+                res.sendFile(path.join(__dirname, 'public/', 'shutdownerror.html'));
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
         });
     });
 }
@@ -47,6 +64,24 @@ app.get('/send', (req, res) => {
     res.sendStatus(200);
 });
 
+app.get('/check', (req, res) => {
+    exec(`nc -vz ${host} 22`, (error, stdout, stderr) => {
+        if (error) {
+            // The PC is off
+            console.log(`error: ${error.message}`);
+            res.sendStatus(502);
+            return;
+        }
+        if (stderr) {
+            // The PC is still on
+            console.log(`stderr: ${stderr}`);
+            res.sendStatus(200);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+});
+
 app.use(stringReplace({
     'redirectWebpage': redirectWebpage,
     'timeToRedirect': timeToRedirect,
@@ -56,3 +91,9 @@ app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
